7f6019ffcdda7f1719caf9fefc717e66db3428d5
bodasooqa <bodasooqa@gmail.com>
2020-02-20 17:23:59 +0300

    Маршрутизация

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,30 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ChartComponent } from './components/chart/chart.component';
 import { PreloaderComponent } from './components/preloader/preloader.component';
+import { TemperatureComponent } from './pages/temperature/temperature.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PrecipitationComponent } from './pages/precipitation/precipitation.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'temperature', pathMatch: 'full' },
+  { path: 'temperature', component: TemperatureComponent },
+  { path: 'precipitation', component: PrecipitationComponent },
+  { path: '**', redirectTo: 'temperature' }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ChartComponent,
-    PreloaderComponent
+    PreloaderComponent,
+    TemperatureComponent,
+    PrecipitationComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
